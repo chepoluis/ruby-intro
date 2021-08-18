11fc5a7e31876df4af18734162d8127265100e0d
Jose  luis Villa <vsjoseluisvs@gmail.com>
2021-08-18 08:02:25 -0500

    Calculate a grade solution

--- challenges/ruby/8-calculate-grade/my_solution.rb
+++ challenges/ruby/8-calculate-grade/my_solution.rb
@@ -2,4 +2,18 @@
 
 # Tu solucion abajo:
 
-
+def get_grade(grade)
+    case grade
+    when 90..100
+        return "A"
+    when 80..90
+        return "B"
+    when 70..80
+        return "C"
+    when 60..70
+        return "D"
+    else
+        return "F"
+    end
+        
+end
